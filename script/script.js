1aa3b0e0135e192606be2c81f5b5e65a32784b70
Anton <bakunin.abolnikov@gmail.com>
2019-12-02 21:44:26 +0200

    Модальное окно 2 Работает

--- script/script.js
+++ script/script.js
@@ -6,23 +6,21 @@ window.addEventListener('DOMContentLoaded', function () {
 // popup phone
 const popupPhone = () => {
     const callBtn = document.querySelectorAll('.call-btn'),
+        popupDiscount = document.querySelector('.popup-discount'),
+        discountBtn = document.querySelectorAll('.discount-btn'),
         popup = document.querySelector('.popup'),
         popupCall = document.querySelector('.popup-call');
 
 
         callBtn.forEach((elem) => {
-
 			elem.addEventListener('click', () => {
                 event.preventDefault();
 
                 popupCall.style.display = 'block';
-                if (windowWidth > 768) {
-				    }
                 });
             });
             
         popup.addEventListener('click', (event) => {
-
             let target = event.target;
     
             if (target.classList.contains('popup-close')) {
@@ -31,6 +29,27 @@ const popupPhone = () => {
                 target = target.closest('.popup-content');
                 if (!target) {
                     popupCall.style.display = 'none';
+                    }
+                }
+            });
+
+        discountBtn.forEach((elem) => {
+			elem.addEventListener('click', () => {
+                event.preventDefault();
+
+                popupDiscount.style.display = 'block';
+                });
+            });
+
+        popupDiscount.addEventListener('click', (event) => {
+            let target = event.target;
+        
+            if (target.classList.contains('popup-close')) {
+                popupDiscount.style.display = 'none';
+            } else {
+                target = target.closest('.popup-content');
+                if (!target) {
+                    popupDiscount.style.display = 'none';
                     }
                 }
             });
