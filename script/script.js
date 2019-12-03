c9e8624c457f7140495a9776336464bfe0c19e67
Anton <bakunin.abolnikov@gmail.com>
2019-12-03 22:08:00 +0200

    Отправка формы + валидация

--- script/script.js
+++ script/script.js
@@ -81,5 +81,87 @@ const popups = () => {
     
     };
             
-    popups();            
+    popups();       
+    
+
+//send-ajax-form
+const sendForm = () => {
+    const errorMessage = 'Что то пошло не так!',
+        loadMessage = 'Загружается...',
+        successMessage = 'Спасибо, мы скоро с Вами свяжемся!';
+
+    const //mainForm = document.querySelector('main-form'),
+        //captureForm = document.querySelectorAll('capture-form'),
+        forms = document.querySelectorAll('form');
+
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 2rem;';
+
+    forms.forEach(form => {
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            form.appendChild(statusMessage);
+            const formData = new FormData(form);
+
+            const body = {};
+
+            formData.forEach((val, key) => {
+                body[key] = val;
+            });
+
+            statusMessage.textContent = loadMessage;
+
+            postData(body)
+                .then(result => {
+                    statusMessage.textContent = result;
+                    form.reset();
+                })
+                .catch(error => statusMessage.textContent = error);
+        });
+    });
+
+    const postData = body => {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4) {
+                    return;
+                }
+                if (request.status === 200) {
+                    resolve(successMessage);
+                } else {
+                    reject(errorMessage);
+                }
+            });
+
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify(body));
+            });
+        };
+    };
+
+    sendForm();
+
+    
+//Validation Inputs
+const validationInput = () => {
+    let inputs = document.querySelectorAll('form');
+    
+    inputs = document.querySelectorAll('input[class = "phone-user"]');
+		inputs.forEach((elem) => {
+			elem.addEventListener('input', () => {
+				elem.value = elem.value.replace(/[^0-9]/, '');
+			});
+		});
+
+	inputs = document.querySelectorAll('input[name = "user_name"]');
+		inputs.forEach((elem) => {
+			elem.addEventListener('input', () => {
+				elem.value = elem.value.replace(/[^а-яА-Я]+$/i, '');
+			});
+		});
+    };
+
+    validationInput();
 });
