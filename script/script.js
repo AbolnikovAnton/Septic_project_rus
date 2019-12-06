0cf55f8922de9ec95b9f06274e30223012af5071
Anton <bakunin.abolnikov@gmail.com>
2019-12-06 22:07:46 +0200

    hiddenBtn

--- script/script.js
+++ script/script.js
@@ -198,4 +198,17 @@ const accordion = () =>{
     };
     
     accordion();
+
+//addButton
+const addButton = () =>{
+    let hiddenBlocks = document.querySelectorAll('.hidden'),
+        hiddenBtn = document.querySelector('.add-sentence-btn');
+
+        hiddenBtn.addEventListener('click', () => {
+            hiddenBlocks.forEach((elem)=>elem.classList.remove('hidden'));
+            hiddenBtn.classList.add('hidden');
+        });
+    };
+
+    addButton();
 });
