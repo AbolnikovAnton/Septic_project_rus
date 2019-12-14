21d2ac0d135beb162534c71acd4c4a25fa00f79d
Anton <bakunin.abolnikov@gmail.com>
2019-12-14 13:54:31 +0200

    рефакторинг попап кода

--- src/modules/popups.js
+++ src/modules/popups.js
@@ -1,78 +1,37 @@
 const popups = () => {
-    const callBtn = document.querySelectorAll('.call-btn'),
-        popupDiscount = document.querySelector('.popup-discount'),
-        discountBtn = document.querySelectorAll('.discount-btn'),
-        popup = document.querySelector('.popup'),
-        checkBtn = document.querySelectorAll('.check-btn'),
-        popupCheck = document.querySelector('.popup-check'),
-        popupCall = document.querySelector('.popup-call');
+    const callBtns = document.querySelectorAll('.call-btn');
+    const discountBtns = document.querySelectorAll('.discount-btn');
+    const checkBtns = document.querySelectorAll('.check-btn');
 
-//call button
-    callBtn.forEach((elem) => {
+    const popups = document.querySelectorAll('.popup');
+    const popupCall = document.querySelector('.popup-call');
+
+    popups.forEach((elem) => {
         elem.addEventListener('click', () => {
-            event.preventDefault();
+            let target = event.target;
 
-            popupCall.style.display = 'block';
-            });
-        });
-        
-    popup.addEventListener('click', (event) => {
-        let target = event.target;
-
-        if (target.classList.contains('popup-close')) {
-            popupCall.style.display = 'none';
-        } else {
-            target = target.closest('.popup-content');
-            if (!target) {
+            if (target.classList.contains('popup-close')) {
                 popupCall.style.display = 'none';
+            } else {
+                target = target.closest('.popup-content');
+                if (!target) {
+                    popupCall.style.display = 'none';
                 }
             }
         });
 
-//discount button
-    discountBtn.forEach((elem) => {
+    });
+
+    const callPopupFunc = (elem) => {
         elem.addEventListener('click', () => {
             event.preventDefault();
-
-            popupDiscount.style.display = 'block';
-            });
+            popupCall.style.display = 'block';
         });
-
-    popupDiscount.addEventListener('click', (event) => {
-        let target = event.target;
-    
-        if (target.classList.contains('popup-close')) {
-            popupDiscount.style.display = 'none';
-        } else {
-            target = target.closest('.popup-content');
-            if (!target) {
-                popupDiscount.style.display = 'none';
-                }
-            }
-        });
-
-//check button
-    checkBtn.forEach((elem) => {
-        elem.addEventListener('click', () => {
-            event.preventDefault();
-
-            popupCheck.style.display = 'block';
-            });
-        });
-
-        popupCheck.addEventListener('click', (event) => {
-        let target = event.target;
-    
-        if (target.classList.contains('popup-close')) {
-            popupCheck.style.display = 'none';
-        } else {
-            target = target.closest('.popup-content');
-            if (!target) {
-                popupCheck.style.display = 'none';
-                }
-            }
-        });
-    
     };
+
+    callBtns.forEach(callPopupFunc);
+    discountBtns.forEach(callPopupFunc);
+    checkBtns.forEach(callPopupFunc);
+};
             
 export default popups;
